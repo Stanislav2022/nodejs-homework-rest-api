09d9b7c344a48b170e5b206f11b91d4bd11c1c2e
Stanislav2022 <tsv5@ukr.net>
2023-05-04 12:06:38 +0300

    router.post

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,23 @@
 const express = require("express");
+const Joi = require("joi");
 
 const contacts = require("../../models/contacts");
 const { HttpError } = require("../../helpers");
 
 const router = express.Router();
 
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
 router.get("/", async (req, res, next) => {
   try {
     const result = await contacts.listContacts();
     res.json(result);
   } catch (error) {
-    res.status(500).json({ message: "Server error" });
+    next(error);
   }
 });
 
@@ -23,13 +30,22 @@ router.get("/:contactId", async (req, res, next) => {
     }
     res.json(result);
   } catch (error) {
-    const { status = 500, message = "Server error" } = error;
-    res.status(status).json({ message });
+    next(error);
   }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = addSchema.validate(req.body);
+    console.log(error);
+    // if (error) {
+    //   throw HttpError(400, error.message);
+    // }
+    // const result = await contacts.addContact(req.body);
+    // res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
