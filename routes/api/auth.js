cbbcbd7ed15af829e24daaa3b828ace5ae47fa8d
Stanislav2022 <tsv5@ukr.net>
2023-05-19 15:12:40 +0300

    add controls

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,3 +1,8 @@
 const express = require("express");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../models/user");
+const ctrl = require("../../controllers/auth");
+
 const router = express.Router();
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 module.exports = router;
