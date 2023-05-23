2b156e2523cd9d43478a1172ea8dd3fc3a71f7bb
Stanislav2022 <tsv5@ukr.net>
2023-05-23 16:07:51 +0300

    register+login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const ctrl = require("../../controllers/auth");
 const { validateBody } = require("../../middlewares");
 const schemas = require("../../models/user");
-const ctrl = require("../../controllers/auth");
 
 const router = express.Router();
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
