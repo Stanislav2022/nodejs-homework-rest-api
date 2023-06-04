b3fa335ef835f976a0a123bd52b12ffcc84a0461
Stanislav2022 <tsv5@ukr.net>
2023-06-04 16:46:25 +0300

    upload

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const isBodyFavorite = require("./isBodyFavorite");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
 module.exports = {
   validateBody,
   isValidId,
   isBodyFavorite,
   authenticate,
+  upload,
 };
